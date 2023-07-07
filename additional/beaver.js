0ebc5646052c9c3b7f29ad214b01b4f22657d78c
Zed-snek <ed.savchuker@gmail.com>
2023-07-07 18:02:32 +0200

    function eat tree

--- additional/beaver.js
+++ additional/beaver.js
@@ -1,6 +1,10 @@
-function isBeaver (entity) {
+function isBeaver(entity) {
     if (entity.toLowerCase() === "beaver") 
         console.log("the beaver")
     else 
         console.log("is not a beaver")
+}
+
+function eatTree() {
+    console.log("TRRRRR")
 }
